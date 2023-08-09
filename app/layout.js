90cc9ee832de8f90517853d9a536ea63bd80fa76
rahul4019 <rahulkashyap4019@gmail.com>
2023-08-09 13:39:22 +0530

    Add favicon

--- app/layout.js
+++ app/layout.js
@@ -10,11 +10,17 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Moviemate',
   description: 'Search movies and series',
+  icons: {
+    icon: ['/favicon.ico'],
+
+  },
 }
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
       <body className={inter.className}>
         <Providers>
           <Header />
